37550187e4883f6fc788c255eff07e0fd5654008
Legend108 <80154701+Legend108@users.noreply.github.com>
2021-10-02 13:38:00 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,68 @@
+module.exports = [
+	{
+		name: "Akinfeev",
+		position: "GK",
+		rating: 67,
+		age: 26,
+	},
+	{
+		name: "Akinfenwa",
+		position: "CF",
+		rating: 70,
+		age: 39,
+	},
+	{
+		name: "Beko",
+		position: "RW",
+		rating: 74,
+		age: 35,
+	},
+	{
+		name: "Brandt",
+		position: "CM",
+		rating: 83,
+		age: 25,
+	},
+	{
+		name: "Guedes",
+		position: "RM",
+		rating: 62,
+		age: 24,
+	},
+	{
+		name: "Holmes",
+		position: "CB",
+		rating: 60,
+		age: 24,
+	},
+	{
+		name: "Kim Yeol",
+		position: "RB",
+		rating: 62,
+		age: 22,
+	},
+	{
+		name: "Lennon",
+		position: "RM",
+		rating: 59,
+		age: 19,
+	},
+	{
+		name: "Perrera",
+		position: "CDM",
+		rating: 78,
+		age: 23,
+	},
+	{
+		name: "Romelo",
+		position: "LW",
+		rating: 56,
+		age: 33,
+	},
+	{
+		name: "Riveldo",
+		position: "CM",
+		rating: 68,
+		age: 30,
+	}
+];
