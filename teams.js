cf50f1d78aff6d58d3f05ea97ff0a93b7fff3898
Legend108 <80154701+Legend108@users.noreply.github.com>
2021-10-02 13:38:23 +0530

    Create teams.js

---
+++ teams.js
@@ -0,0 +1,249 @@
+module.exports = [{
+  name: "FC Shakhtar Donetsk",
+  country: "Ukraine",
+  rating: 81,
+  wikipedia: "/wiki/FC_Shakhtar_Donetsk",
+  twitter: "@FCShakhtar",
+  facebook: "/fcshakhtar",
+  reddit: "/r/fcshakhtar"
+}, {
+  name: "Malmö FF",
+  country: "Sweden",
+  rating: 75,
+  wikipedia: "/wiki/Malmö_FF",
+  twitter: "@Malmo_FF",
+  facebook: "/malmoff",
+  reddit: "/r/malmoff"
+}, {
+  name: "Paris Saint-Germain",
+  country: "France",
+  rating: 91,
+  wikipedia: "/wiki/Paris_Saint-Germain_F.C.",
+  twitter: "@PSG_inside",
+  facebook: "/PSG",
+  reddit: "/r/PSG"
+}, {
+  name: "Real Madrid CF",
+  country: "Spain",
+  rating: 89,
+  wikipedia: "/wiki/Real_Madrid_C.F.",
+  twitter: "@realmadrid",
+  facebook: "/RealMadrid",
+  reddit: "/r/RealMadrid"
+}, {
+  name: "Manchester United FC",
+  country: "England",
+  rating: 86,
+  wikipedia: "/wiki/Manchester_United_F.C.",
+  twitter: "@ManUtd",
+  facebook: "/The-Red-Devils",
+  reddit: "/r/reddevils"
+ }, {
+  name: "PFC CSKA Moskva",
+  country: "Russia",
+  rating: 78,
+  wikipedia: "/wiki/PFC_CSKA_Moscow",
+  twitter: "@PFC_CSKA_MOSCOW",
+  facebook: "/PFCCSKAMOSCOW",
+  reddit: ""
+ }, {
+  name: "PSV Eindhoven",
+  country: "Netherlands",
+  rating: 79,
+  wikipedia: "/wiki/PSV_Eindhoven",
+  twitter: "@PSV",
+  facebook: "/PSV",
+  reddit: "/r/psv"
+}, {
+  name: "VfL Wolfsburg",
+  country: "Germany",
+  rating: 83,
+  wikipedia: "/wiki/VfL_Wolfsburg",
+  twitter: "@VfL_Wolfsburg",
+  facebook: "/vflwolfsburg.int",
+  reddit: "/r/diewolfe"
+}, {
+  name: "Club Atlético de Madrid",
+  country: "Spain",
+  rating: 91,
+  wikipedia: "/wiki/Atlético_Madrid",
+  twitter: "@Atleti",
+  facebook: "/AtleticodeMadrid",
+  reddit: "/r/atletico"
+}, {
+  name: "FC Astana",
+  country: "Kazakhstan",
+  rating: 70,
+  wikipedia: "/wiki/FC_Astana",
+  twitter: "@fc_astana",
+  facebook: "",
+  reddit: ""
+ }, {
+  name: "Galatasaray AŞ",
+  country: "Turkey",
+  rating: 73,
+  wikipedia: "/wiki/Galatasaray_S.K._(football)",
+  twitter: "@GalatasaraySK",
+  facebook: "/Galatasaray",
+  reddit: "/r/galatasaray"
+}, {
+  name: "SL Benfica",
+  country: "Portugal",
+  rating: 84,
+  wikipedia: "/wiki/S.L._Benfica",
+  twitter: "@SL_Benfica",
+  facebook: "/SLBenfica",
+  reddit: "/r/benfica",
+}, {
+  name: "Juventus",
+  country: "Italy",
+  rating: 88,
+  wikipedia: "/wiki/Juventus_F.C.",
+  twitter: "@juventusfc",
+  facebook: "/Juventus",
+  reddit: "/r/Juve",
+}, {
+  name: "Manchester City FC",
+  country: "England",
+  rating: 91,
+  wikipedia: "/wiki/Manchester_City_F.C",
+  twitter: "@MCFC",
+  facebook: "/mcfcofficial",
+  reddit: "/r/MCFC",
+},{
+  name: "Sevilla FC",
+  country: "Spain",
+  rating: 84,
+  wikipedia: "/wiki/Sevilla_FC",
+  twitter: "@SevillaFC",
+  facebook: "/sevillafc",
+  reddit: "/r/SevillaFC",
+}, {
+  name: "VfL Borussia Mönchengladbach",
+  country: "Germany",
+  rating: 83,
+  wikipedia: "/wiki/Borussia_Mönchengladbach",
+  twitter: "@borussia",
+  facebook: "/borussia.mg",
+  reddit: "/r/fohlenelf",
+}, {
+  name: "AS Roma",
+  country: "Italy",
+  rating: 83,
+  wikipedia: "/wiki/A.S._Roma",
+  twitter: "@OfficialASRoma",
+  facebook: "/officialasroma",
+  reddit: "/r/ASRoma",
+}, {
+  name: "Bayer 04 Leverkusen",
+  country: "Germany",
+  rating: 82,
+  wikipedia: "/wiki/Bayer_04_Leverkusen",
+  twitter: "@bayer04fussball",
+  facebook: "/bayer04fussball",
+  reddit: "/r/Bayer04",
+}, {
+  name: "FC Barcelona",
+  country: "Spain",
+  rating: 89,
+  wikipedia: "/wiki/FC_Barcelona",
+  twitter: "@FCBarcelona",
+  facebook: "/fcbarcelona",
+  reddit: "/r/Barca",
+}, {
+  name: "FC BATE Borisov",
+  country: "Belarus",
+  rating: 80,
+  wikipedia: "/wiki/FC_BATE_Borisov",
+  twitter: "@fcbate",
+  facebook: "/fcbate/",
+  reddit: "",
+}, {
+  name: "Arsenal FC",
+  country: "England",
+  rating: 85,
+  wikipedia: "/wiki/Arsenal_F.C.",
+  twitter: "@Arsenal",
+  facebook: "/Arsenal",
+  reddit: "/r/Gunners",
+}, {
+  name: "FC Bayern München",
+  country: "Germany",
+  rating: 89,
+  wikipedia: "/wiki/FC_Bayern_Munich",
+  twitter: "@FCBayern",
+  facebook: "/FCBayern",
+  reddit: "/r/fcbayern",
+}, {
+  name: "GNK Dinamo Zagreb",
+  country: "Croatia",
+  rating: 72,
+  wikipedia: "/wiki/GNK_Dinamo_Zagreb",
+  twitter: "@GNKDinamo",
+  facebook: "/dinamo",
+  reddit: ""
+}, {
+  name: "Olympiacos FC",
+  country: "Greece",
+  rating: 79,
+  wikipedia: "/wiki/Olympiacos_F.C.",
+  twitter: "@olympiacos_org",
+  facebook: "/Olympiacos_F.C.",
+  reddit: ""
+}, {
+  name: "Chelsea FC",
+  country: "England",
+  rating: 88,
+  wikipedia: "/wiki/Chelsea_F.C.",
+  twitter: "@chelseafc",
+  facebook: "/ChelseaFC",
+  reddit: "/r/chelseafc"
+}, {
+  name: "FC Dynamo Kyiv",
+  country: "Ukraine",
+  rating: 78,
+  wikipedia: "FC_Dynamo_Kyiv",
+  twitter: "@dynamokofficial",
+  facebook: "/fcdynamoua",
+  reddit: ""
+}, {
+  name: "FC Porto",
+  country: "Portugal",
+  rating: 82,
+  wikipedia: "/wiki/FC_Porto",
+  twitter: "@FCPorto",
+  facebook: "/FCPorto",
+  reddit: "/r/fcporto"
+}, {
+  name: "Maccabi Tel-Aviv FC",
+  country: "Israel",
+  rating: 69,
+  wikipedia: "/wiki/Maccabi_Tel_Aviv_F.C.",
+  twitter: "@hapoeltelavivfc",
+  facebook: "/MaccabiTLVFC",
+  reddit: ""
+}, {
+  name: "KAA Gent",
+  country: "Belgium",
+  rating: 76,
+  wikipedia: "/wiki/K.A.A._Gent",
+  twitter: "@KAAGent",
+  facebook: "/kaagent",
+  reddit: "",
+}, {
+  name: "Olympique Lyonnais",
+  country: "France",
+  rating: 85,
+  wikipedia: "/wiki/Olympique_Lyonnais",
+  twitter: "@OL",
+  facebook: "/olympiquelyonnais",
+  reddit: "/r/olympiquelyonnais/"
+}, {
+  name: "Valencia CF",
+  country: "Spain",
+  rating: 83,
+  wikipedia: "/wiki/Valencia_CF",
+  twitter: "@valenciacf",
+  facebook: "/ValenciaCF",
+  reddit: "/r/ValenciaCF"
+}];
