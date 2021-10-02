7f3476c6754d46a6aad39c5d36c3cd1eb330565c
Legend108 <80154701+Legend108@users.noreply.github.com>
2021-10-02 13:43:37 +0530

    Update ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,22 +1,27 @@
-const { MessageEmbed } = require('discord.js')
+const { MessageEmbed } = require("discord.js");
 module.exports = {
-    name : 'ping',
-    category : 'info',
-    description : 'Returns latency and API ping',
+  name: "ping",
+  category: "info",
+  description: "Returns latency and API ping",
 
-    /**
-     * @param {Client} client
-     * @param {Message} message
-     * @param {String[]} args
-     */
+  /**
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   */
 
-    run : async(client, message, args) => {
-        const msg = await message.channel.send(`🏓 Pinging...`)
-        const embed = new MessageEmbed()
-            .setTitle('Pong!')
-            .setDescription(`WebSocket ping is ${client.ws.ping}MS\nMessage edit ping is ${Math.floor(msg.createdAt - message.createdAt)}MS!`)
-            await message.channel.send(embed)
-            msg.delete()
-
-    }
-}
+  run: async (client, message, args) => {
+    const msg = await message.channel.send(`🏓 Pinging...`);
+    const embed = new MessageEmbed()
+      .setTitle("Pong!")
+      .setDescription(
+        `WebSocket ping is ${
+          client.ws.ping
+        }MS\nMessage edit ping is ${Math.floor(
+          msg.createdAt - message.createdAt
+        )}MS!`
+      );
+    await message.channel.send(embed);
+    msg.delete();
+  },
+};
